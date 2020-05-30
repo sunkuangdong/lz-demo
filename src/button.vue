3dd1744ba6de780bad285c67e06903efdad3ae17
褚煦露 <chuxulu@lezhixing.com.cn>
2020-05-30 21:50:11 +0800

    parcel问题

---
+++ src/button.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button class="g-button">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style scoped lang='scss'>
+.g-button {
+  font-size: 14px;
+}
+</style>
